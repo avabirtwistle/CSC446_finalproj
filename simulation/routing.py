d81feecb4216d00fe676a2084a57252ffe84ca61
Ava Birtwistle <148493409+avabirtwistle@users.noreply.github.com>
2025-11-29 13:16:13 -0800

    Refactor Routing class constructor
    Removed unused attributes from the Routing class.

--- simulation/routing.py
+++ simulation/routing.py
@@ -2,6 +2,5 @@
 
 class Routing:
     def __init__(self, car, routing_policy):
-        self.station_arrival_time = None # time car arrives at station, none indicates in queue or driving
-        self.queue_entry_time = None # time car enters queue at station, none indicates not in queue
-        self.station_routed_to = None # the station meta or station not sure
+        self.car = car
+        self.routing_policy = routing_policy
